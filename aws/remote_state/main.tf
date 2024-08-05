664cc3aefdcde151238cd50f1b72604780f0e6b0
RAHUL_DA <143429212+JasonD001@users.noreply.github.com>
2024-08-05 16:54:30 +0530

    Update main.tf

--- aws/remote_state/main.tf
+++ aws/remote_state/main.tf
@@ -1,39 +1,24 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# CREATE AN S3 BUCKET AND DYNAMODB TABLE TO USE AS A TERRAFORM BACKEND
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-# ----------------------------------------------------------------------------------------------------------------------
-# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
-# This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
-# This module is forked from https://github.com/gruntwork-io/intro-to-terraform/tree/master/s3-backend
-# ----------------------------------------------------------------------------------------------------------------------
-
 terraform {
   required_version = ">= 0.12"
 }
 
-# ------------------------------------------------------------------------------
-# CONFIGURE OUR AWS CONNECTION
-# ------------------------------------------------------------------------------
-
-provider "aws" {}
-
-# ------------------------------------------------------------------------------
-# CREATE THE S3 BUCKET
-# ------------------------------------------------------------------------------
-
-data "aws_caller_identity" "current" {}
-
-locals {
-  account_id    = data.aws_caller_identity.current.account_id
+provider "aws" {
+  region = "ap-south-1"  # Specify your AWS region
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  # With account id, this S3 bucket names can be *globally* unique.
-  bucket = "${local.account_id}-terraform-states"
+# Retrieve current AWS account details
+data "aws_caller_identity" "current" {}
 
-  # Enable versioning so we can see the full revision history of our
-  # state files
+# Local variable for the account ID
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+}
+
+# Create the S3 bucket for Terraform state
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "${local.account_id}-terraform-states"
+  
+  # Enable versioning for the bucket
   versioning {
     enabled = true
   }
@@ -48,17 +33,34 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
-# ------------------------------------------------------------------------------
-# CREATE THE DYNAMODB TABLE
-# ------------------------------------------------------------------------------
-
+# Create DynamoDB table for state locking
 resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "terraform-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
+  name           = "terraform-lock"
+  billing_mode    = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+  
   attribute {
     name = "LockID"
     type = "S"
   }
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.terraform_state.bucket
+}
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.terraform_state.arn
+}
+
+output "s3_bucket_region" {
+  value = aws_s3_bucket.terraform_state.region
+}
+
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.terraform_lock.name
+}
+
+output "dynamodb_table_arn" {
+  value = aws_dynamodb_table.terraform_lock.arn
 }
